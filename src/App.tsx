df4941847c82d4ec9dc6149d046c7ff05c8e1b9a
Nascccom <ostrov_o@bk.ru>
2023-08-22 21:27:29 +0300

    Created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import {Header} from "./components/Header.tsx";
+import {Header} from "./components/Header/Header.tsx";
 import {Footer} from "./components/Footer.tsx";
 import {Nav} from "./components/Section/Nav.tsx";
-import {Main} from "./components/Section/Main.tsx";
-import {Contacts} from "./components/Section/Contacts.tsx";
+import {Main} from "./components/Section/Main/Main.tsx";
+import {Contacts} from "./components/Section/Contacts/Contacts.tsx";
 
 export const App = () => {
+    // const dispatch = useAppDispatch()
 
     return (
       <div className="App">
