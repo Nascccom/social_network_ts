85480bd0bbebcd47f68dd5503eb64c08e4592aa0
Nascccom <ostrov_o@bk.ru>
2023-08-25 15:17:25 +0300

    Grid page layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import './App.css'
 import {Header} from "./components/Header/Header.tsx";
 import {Nav} from "./components/Section/Nav/Nav.tsx";
 import {Main} from "./components/Section/Main/Main.tsx";
+import {Contacts} from "./components/Section/Contacts/Contacts.tsx";
 
 export const App = () => {
     // const dispatch = useAppDispatch()
 
+    // const [section, setSection] = useState<SectionCSSType>("sectionAll")
+
     return (
       <div className="App">
-          <Header/>
-          <div className={'section'}>
-              <Nav/>
-              <Main/>
-              {/*<Contacts/>*/}
+          <Header />
+          <div className={'sectionAll'}>
+              <Nav />
+              <Main />
+              <Contacts/>
           </div>
           {/*<Footer/>*/}
       </div>
-
     )
 }
+
+export type SectionCSSType =
+  | "sectionAll"
+  | "sectionMessages"
